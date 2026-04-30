d43c0799807f5ad7f2856a15fe8d367b3cbe8f61
phucdai300307-glitch <phucdai300307@gmail.com>
2026-04-30 18:04:39 +0700

    sua lai code sau khi test thu va da khong cho merge khi loi

--- src/main/java/com/math/MathUtils.java
+++ src/main/java/com/math/MathUtils.java
@@ -3,17 +3,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.*;
 
 
 public class MathUtils {
     private static final Logger logger = LoggerFactory.getLogger(MathUtils.class);
-    public static final int so_nguyen_mau = 10;
-    public void Tinh_Toan() {};
 
     public static String getFilePath() {
-        int so_A = 10;
         // Không dùng "C:\\..." nữa. Paths.get sẽ tự thêm / hoặc \ tùy vào hệ điều hành
         Path path = Paths.get("data", "temp", "config.txt");
         return path.toString();
